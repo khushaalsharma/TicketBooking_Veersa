987099f1dffc1fcb1dfac29b8185d7c21b569b1c
Khushaal Sharma <khushaalsharma1@gmail.com>
2024-10-23 15:49:14 +0530

    corrected edit profile

--- TicketBooking_WebAPI/Models/Domain/Payments.cs
+++ TicketBooking_WebAPI/Models/Domain/Payments.cs
@@ -7,6 +7,7 @@ namespace TicketBooking_WebAPI.Models.Domain
         public Guid Id { get; set; }
         public string PaymentMethod { get; set; }
         public string MethodDetail { get; set; } //takes card number or apple ID
+        //public DateTime CreatedAt { get; set; }
         public int Amount { get; set; }
         [ForeignKey(nameof(User))]
         public string UserId { get; set; }
